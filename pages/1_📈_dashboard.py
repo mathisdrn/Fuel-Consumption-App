33099256d9cc335c54ea1db512fbf4daf77ff2e2
MathisDrn <116900975+mathisdrn@users.noreply.github.com>
2025-01-24 04:01:23 +0100

    dashboard migration to polars

--- pages/1_📈_dashboard.py
+++ pages/1_📈_dashboard.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import altair as alt
 import pandas as pd
+import polars as pl
 from utils import get_car_data, percentage_change
 
 st.set_page_config(
@@ -13,34 +14,46 @@ st.title("📈 Dashboard")
 
 df = get_car_data() 
 
-# Convert to Pandas DataFrame
-df = df.to_pandas()
-
 # -------------- Metric --------------
 
 # Group by year and calculate the required metrics
-metrics_df = df.groupby('release_year').agg(
-    model_count=('make', 'count'),
-    mixed_consumption=('fc_mixed', 'mean'),
-    emissions=('emissions', 'mean')
-).reset_index()
-
-# Extract metrics for 2021 and 2022
-m_2021 = metrics_df[metrics_df['release_year'] == 2021].iloc[0]
-m_2022 = metrics_df[metrics_df['release_year'] == 2022].iloc[0]
+metrics = df.group_by('release_year').agg([
+    pl.col('make').count().alias('model_count'),
+    pl.col('fc_mixed').mean().alias('fc_mixed'),
+    pl.col('emissions').mean().alias('emissions')
+  ]).sort('release_year', descending=True)
 
 # Display metrics
 st.header("Overview - Year on Year")
 
 col1, col2, col3 = st.columns(3)
-col1.metric("Number of models", f"{m_2021['model_count']:.0f}", f"{percentage_change(m_2022['model_count'], m_2021['model_count']):.1%}")
-col2.metric("Mixed consumption (L/100km)", f"{m_2022['mixed_consumption']:.1f} L", f"{m_2022['mixed_consumption'] - m_2021['mixed_consumption']:.2f} L", delta_color='inverse')
-col3.metric("Vehicle emissions", f"{m_2022['emissions']:.0f} g/km", f"{percentage_change(m_2022['emissions'], m_2021['emissions']):.2%}", delta_color='inverse')
+
+col1.metric(
+    label = "Number of models", 
+    value = f"{metrics[0, 'model_count']:.0f}",
+    delta = f"{percentage_change(metrics[0, 'model_count'], metrics[1, 'model_count']):.1%}"
+    )
+
+col2.metric(
+    label = "Mixed consumption (L/100km)",
+    value = f"{metrics[0, 'fc_mixed']:.1f} L",
+    delta = f"{metrics[0, 'fc_mixed'] - metrics[1, 'fc_mixed']:.2f} L",
+    delta_color='inverse'
+    )
+
+col3.metric(
+    label = "Vehicle emissions",
+    value = f"{metrics[0, 'emissions']:.0f} g/km",
+    delta = f"{percentage_change(metrics[0, 'emissions'], metrics[1, 'emissions']):.2%}",
+    delta_color='inverse'
+)
 
 # ------------- Plotting -------------
 
-# Number of models released over time
-temp = df.groupby('release_year').size().reset_index(name='model_count')
+# Model count over time
+temp = df.group_by('release_year').agg([
+    pl.col('make').count().alias('model_count')
+])
 
 chart = alt.Chart(temp).mark_line().encode(
     x=alt.X('release_year:O', title='Year', axis=alt.Axis(labelAngle=0, values=temp['release_year'].unique()[::2])),
@@ -51,9 +64,12 @@ chart = alt.Chart(temp).mark_line().encode(
 ).properties(title='Number of models released over time')
 st.altair_chart(chart, use_container_width=True)
 
-# Vehicle class proportion over time
-temp = df.groupby(['release_year', 'vehicle_class']).size().reset_index(name='model_count')
-temp['proportion'] = temp['model_count'] / temp.groupby('release_year')['model_count'].transform('sum') 
+# Proportion of vehicle class over time
+temp = df.group_by(['release_year', 'vehicle_class']).agg([
+    pl.col('make').count().alias('model_count')
+]).with_columns([
+    (pl.col('model_count') / pl.col('model_count').sum().over('release_year')).alias('proportion')
+])
 
 chart = alt.Chart(temp).mark_area().encode(
     x=alt.X('release_year:O', title='Year', axis=alt.Axis(labelAngle=0, values=temp['release_year'].unique()[::2])),
@@ -66,9 +82,12 @@ chart = alt.Chart(temp).mark_area().encode(
 ).properties(title='Vehicle Class Proportion Over Time')
 st.altair_chart(chart, use_container_width=True)
 
-# Vehicle fuel type proportion over time
-temp = df.groupby(['release_year', 'fuel_type']).size().reset_index(name='model_count')
-temp['proportion'] = temp['model_count'] / temp.groupby('release_year')['model_count'].transform('sum')
+# Proportion of vehicle fuel type over time
+temp = df.group_by(['release_year', 'fuel_type']).agg([
+    pl.col('make').count().alias('model_count')
+]).with_columns([
+    (pl.col('model_count') / pl.col('model_count').sum().over('release_year')).alias('proportion')
+])
 
 chart_fuel = alt.Chart(temp).mark_bar().encode(
     x=alt.X('release_year:O', title='Year', axis=alt.Axis(labelAngle=0, values=temp['release_year'].unique()[::2])),
@@ -82,10 +101,16 @@ chart_fuel = alt.Chart(temp).mark_bar().encode(
 st.altair_chart(chart_fuel, use_container_width=True)
 
 # Fuel type proportion versus vehicle type
-temp = df.groupby(['vehicle_class', 'fuel_type']).size().reset_index(name='model_count')
-temp['proportion'] = temp['model_count'] / temp.groupby('vehicle_class')['model_count'].transform('sum')
+temp = df.group_by(['vehicle_class', 'fuel_type']).agg([
+    pl.col('make').count().alias('model_count')
+]).with_columns([
+    (pl.col('model_count') / pl.col('model_count').sum().over('vehicle_class')).alias('proportion')
+])
 
-order = df.groupby(['vehicle_class'])['make'].count().sort_values(ascending=False).index.to_list()
+# Order vehicle classes by model count
+order = df.group_by('vehicle_class').agg([
+    pl.col('make').count().alias('model_count')
+]).sort('model_count', descending=True)['vehicle_class'].to_list()
 
 chart_fuel = alt.Chart(temp).mark_bar().encode(
     x=alt.X('proportion:Q', title='Fuel Type Proportion', axis=alt.Axis(format='%')),
@@ -104,8 +129,9 @@ st.markdown('### Vehicle emissions')
 # Vehicle emissions over time for a specific fuel type
 fuel_type = st.selectbox('Select a fuel type', df['fuel_type'].unique(), index=0)
 
-temp = df[df['fuel_type'] == fuel_type]
-temp = temp.groupby(['release_year', 'vehicle_class'])['emissions'].mean().reset_index()
+temp = df.filter(pl.col('fuel_type') == fuel_type).group_by(['release_year', 'vehicle_class']).agg([
+    pl.col('emissions').mean().alias('emissions')
+])
 
 chart_emissions = alt.Chart(temp).mark_line().encode(
     x=alt.X('release_year:O', title='Year', axis=alt.Axis(labelAngle=0, values=temp['release_year'].unique()[::2])),
@@ -121,8 +147,9 @@ st.altair_chart(chart_emissions, use_container_width=True)
 # Vehicle emissions over time for a specific vehicle class
 vehicle_class = st.selectbox('Select a vehicle type', df['vehicle_class'].unique(), index=0)
 
-temp = df[df['vehicle_class'] == vehicle_class]
-temp = temp.groupby(['release_year', 'fuel_type'])['emissions'].mean().reset_index()
+temp = df.filter(pl.col('vehicle_class') == vehicle_class).group_by(['release_year', 'fuel_type']).agg([
+    pl.col('emissions').mean().alias('emissions')
+])
 
 chart_emissions = alt.Chart(temp).mark_line().encode(
     x=alt.X('release_year:O', title='Year', axis=alt.Axis(labelAngle=0, values=temp['release_year'].unique()[::2])),
